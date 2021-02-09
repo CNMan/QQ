72ab6042c0b0b68a323dbb66707e0a19a78c357b
CNMan <nklijl@mail.nankai.edu.cn>
2021-02-09 12:35:51 +0800

    Tue 09 Feb 2021 12:35:51 PM HKT

---
+++ update.sh
@@ -0,0 +1,5 @@
+wget -N https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2020.exe
+wget -N https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2021.exe
+git add .
+git commit -m "`date`"
+git push -f origin master
